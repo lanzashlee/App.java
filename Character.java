c4bee053dd5742836368432427488a58b391f87a
Lanz Ashlee Ricamara <111546362+lanzashlee@users.noreply.github.com>
2022-09-02 19:08:53 +0800

    Create Character.java

---
+++ Character.java
@@ -0,0 +1,23 @@
+public class Character {
+        int strength;
+        int agility;
+        int intelligence;
+        String name;
+        
+     // Character method
+     public void sayMyStrength() {
+        System.out.println("My itrength is " + strength);
+    }
+         // Character method
+    public void sayMyAgility(){                                   
+        System.out.println("My igility is " + agility);
+    }
+     // Character method
+     public void sayMyIntelligence() {
+        System.out.println("My intelligence is " + intelligence);
+     }
+           // Character method
+    public void sayMyName() {
+        System.out.println("Hello I am " + name);
+    }
+    }
