27b50cd52ca60307cbcc0cd983f8db7270133f1c
Lanz Ashlee Ricamara <111546362+lanzashlee@users.noreply.github.com>
2022-09-03 14:37:14 +0800

    Create Pet.java

---
+++ Pet.java
@@ -0,0 +1,26 @@
+public class Pet {
+    // Properties
+    // name
+    String name;
+
+    // trust level
+    int trustlevel;
+
+    // isAdopted
+    boolean isAdopted;
+
+    // Birthday
+    // 05/17/2007
+    String birthday;
+
+    //Pet method
+    public void itsMyPet(){
+        System.out.println("Hi my name is" + name);
+        System.out.println("My trust level to my master is" + trustlevel);
+        System.out.println(isAdopted);
+        System.out.println("My birthday is" + birthday);
+
+
+    }
+    
+}
