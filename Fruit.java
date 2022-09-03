7736f481de5c876e73db99816168de6c6c63a7b1
Lanz Ashlee Ricamara <111546362+lanzashlee@users.noreply.github.com>
2022-09-03 14:37:59 +0800

    Create Fruit.java

---
+++ Fruit.java
@@ -0,0 +1,25 @@
+public class Fruit{
+    //properties
+    //object - Banana
+    // color - Yellow
+    String color;
+    //taste - supersweet
+    String taste;
+    //weight - 0.50
+    float weight;
+    //isMyFavorite - true
+    boolean isMyFavorite;
+    //firstLetterofTheObject - B
+    char firstLetterofTheObject;
+
+    //Fruit mrthod
+    public void fruit(){
+        System.out.println("banana color" + color);
+        System.out.println("banana taste" + taste);
+        System.out.println("banana weight" + weight);
+        System.out.println("its my favorite" + isMyFavorite);
+        System.out.println(firstLetterofTheObject);
+    }
+    
+
+}
