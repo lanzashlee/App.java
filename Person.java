59d5c4c34f8b5a54e34eca8b0d313bc43b8a818d
Lanz Ashlee Ricamara <111546362+lanzashlee@users.noreply.github.com>
2022-09-05 22:26:14 +0800

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    //student properties
+    String surname;
+    String firstName;
+    char middleinitial;
+    int studentNumber;
+    String dateOfBirth;
+    String studentEmailAddress;
+    boolean iAmAwesome;
+
+    // Student Method
+    public void Me(){
+        System.out.println("My name is " + surname);
+        System.out.println(firstName);
+        System.out.println(middleinitial);
+        System.out.println(dateOfBirth);
+        System.out.println(studentNumber);
+        System.out.println( studentEmailAddress);
+        System.out.println( iAmAwesome);
+    }
+
+
+}
